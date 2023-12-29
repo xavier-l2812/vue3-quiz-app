80849c4896d49ffd8ec84bbcf32ebc0e1db63ec3
Xavier <xli@cirrusinsight.com>
2023-12-28 16:49:23 -0800

    initial version of the quiz app

---
+++ src/App.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <RouterView />
+  </div>
+</template>
+
+<script setup>
+  import {RouterView} from "vue-router"
+</script>
+
+<style scoped>
+  .container {
+    max-width: 1000px;
+    margin: 0 auto;
+  }
+</style>
---
+++ src/components/QuizHeader.vue
@@ -0,0 +1,36 @@
+<template>
+  <header>
+    <h4>Question {{ questionStatus }}</h4>
+    <div class="bar">
+      <div class="completion" :style="{width: barPercentage}"></div>
+    </div>
+  </header>
+</template>
+
+<script setup>
+  import { defineProps } from 'vue';
+
+  const {questionStatus, barPercentage} = defineProps(['questionStatus', 'barPercentage'])
+</script>
+
+<style scoped>
+  header {
+    margin-top: 20px;
+  }
+
+  header h4 {
+    font-size: 30px;
+  }
+
+  .bar {
+    width: 300px;
+    height: 50px;
+    border: 3px solid bisque
+  }
+
+  .completion {
+    height: 100%;
+    width: 0%;
+    background-color: bisque;
+  }
+</style>
